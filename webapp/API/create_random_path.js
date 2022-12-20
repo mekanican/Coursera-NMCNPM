3f62bdd041dfeb022a9d65f27ca1fa86d956cd61
mekanican <boommerang73@gmail.com>
2022-12-20 16:16:27 +0700

    random path

---
+++ webapp/API/create_random_path.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function randomString(length) {
+    let dictionary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let dict_length = dictionary.length;
+
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        result += dictionary.charAt(Math.floor(Math.random() * dict_length));
+    }
+    return result;
+}
+
+function getRandomPath() {
+    const baseDataPath = '/course-data/';
+    return baseDataPath + randomString(20);
+}
+
+function getExtensionFromPath(path) {
+    return path.split('.').pop();
+}
+
+function getRandomPathFromFile(origPath) {
+    let ext = getExtensionFromPath(origPath);
+    let newPath = getRandomPath() + '.' + ext;
+    fs.copyFileSync(origPath, newPath, fs.constants.COPYFILE_EXCL);
+    return newPath;
+}
+
+module.exports = {getRandomPathFromFile, getRandomPath};
