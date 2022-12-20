5988a694983a1e48fce930727e832f472df6ed82
phphuc62 <20120351@student.hcmus.edu.vn>
2022-12-20 09:24:49 +0700

    Update certificate

--- webapp/controllers/certificate.controller.js
+++ webapp/controllers/certificate.controller.js
@@ -3,94 +3,13 @@ const { default: mongoose } = require('mongoose');
 
 // Declare model
 const {Certificate} = require('../models/certificate');
-const {User, CourseInformation} = require('../models/temp_user');
 
-
-// TODO: this is test function, delete after use
-// route: /testget
-const getGoals = asyncHandler(async (req, res) => {
-  // Create a new record 
-  // await createCertificate(
-  //   mongoose.Types.ObjectId('639f5509965e46582a4447d2'),
-  //   mongoose.Types.ObjectId('639bed9aaf8d87cdcda35be9'),
-  //   "abcd123",
-  //   (err, object) => {
-  //     if (!err){
-  //       console.log("Create successfully")
-  //       console.log(object.toString())
-  //     } else {
-  //       console.log(err)
-  //     }
-  //   }
-  //   )
-
-  // Find and delete a record
-  //  await getCertificate(
-  //       mongoose.Types.ObjectId('639e4dddca85db4b1bae53ab'),
-  //       mongoose.Types.ObjectId('639e4f1eca85db4b1bae53af'),
-  //       (errorMessage, object) => {
-  //         if (!errorMessage) {
-  //           console.log(object._id.toString())
-  //           deleteCertificate(
-  //             object._id,
-  //             (err, foundCertificate) => {
-  //               if (!err) {
-  //                 console.log('Delete successfully')
-  //                 console.log(foundCertificate.toString())
-  //               } else {
-  //                 console.log(err)
-  //                 console.log('No certificate has been found')
-  //               }
-  //             }
-  //           )
-  //         } else {
-  //           console.log("Error")
-  //         }
-  //       }
-  //   )
-
-  // Check reference document
-  // await getCertificate(
-  //   mongoose.Types.ObjectId('639f5509965e46582a4447d2'),
-  //   mongoose.Types.ObjectId('639bed9aaf8d87cdcda35be9'),
-  //   (error, certificate) => {
-  //     if (error) {
-  //       console.log(error);
-  //       return;
-  //     }
-  //     console.log(certificate)
-  //     certificate.populate('UserId').then(
-  //       (certificate) => {
-  //         console.log(certificate.UserId)
-  //       },
-  //       (error) => {
-  //         console.log('Error happened in population')
-  //         console.log(error)
-  //       }
-  //     )
-  //   }
-  // )
-
-  // Test update
-  // await updateCertificateImage(
-  //   mongoose.Types.ObjectId('639f557b43de820e3882de7a'),
-  //   'new image uri',
-  //   (error, newCertificate) => {
-  //     if (error) {
-  //       console.log(error)
-  //     } else {
-  //       console.log(newCertificate)
-  //     }
-  //   })
-
-  res.status(200).json('OKE');
-}) 
-
-const getCertificate = (async (userId, courseId, callback) => {
+const get = (async (userId, courseId, callback) => {
   await Certificate.findOne(
     {
       "UserId": userId,
-      "CourseId": courseId
+      "CourseId": courseId,
+      "IsDeleted": false
     }
   ).then(
     (certificate) => {
@@ -105,15 +24,15 @@ const getCertificate = (async (userId, courseId, callback) => {
   )
 })
 
-const createCertificate = (async (userId, courseId, certificateImage, callback) => {
+const create = (async (userId, courseId, certificateImage, callback) => {
   await Certificate.create({
     "DateComplete": Date.now(),
     "UserId": userId,
     "CourseId": courseId,
-    "CertificateURI": certificateImage
+    "CertificateURI": certificateImage,
+    "IsDeleted": false
   }).then(
     (certificate) => {
-
         callback(null, certificate)
     },
     (error) => {
@@ -122,25 +41,35 @@ const createCertificate = (async (userId, courseId, certificateImage, callback) 
   )
 })
 
-const updateCertificateImage = (async (certificateId, image, callback) => {
-  await Certificate.findByIdAndUpdate(certificateId, 
-    { "CertificateURI": image}
-    ).then(
-      (foundCertificate) => {
-        return callback(null, foundCertificate)
-      },
-      (error) => {
-        return callback(error)
-      }
-    )
+const updateImage = (async (userId, courseId, image, callback) => {
+  var callbackInGet = (error, certificate) => {
+    if (!error) {
+      certificate.update(
+        {
+          "CertificateURI": image
+        }
+      )
+      callback(null, certificate)
+    } else {
+      callback(error)
+    }
+  }
+
+  await get(userId, courseId, callbackInGet)
 })
 
-const deleteCertificate = (async (certificateId, callback) => {
-  await Certificate.findByIdAndDelete(certificateId).then(
+const deleteSoft = (async (certificateId, callback) => {
+  await Certificate.findOne(
+    {
+      "CertificateId": certificateId,
+      "IsDeleted": false
+    }).then(
     (foundCertificate) => {
       if (!foundCertificate){
         return callback(`Certificate ${certificateId.toString()} cannot be found`)
       } else {
+        foundCertificate.IsDeleted = true
+        foundCertificate.save()
         return callback(null, foundCertificate)
       }
     },
@@ -151,9 +80,8 @@ const deleteCertificate = (async (certificateId, callback) => {
 })
 
 module.exports = {
-  getGoals,
-  createCertificate,
-  getCertificate,
-  updateCertificateImage,
-  deleteCertificate
+  get,
+  create,
+  updateImage,
+  deleteSoft
 }
--- webapp/routes/certificate.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {
-  getGoals
-} = require('../controllers/certificate.controller');
-
-router.route('/testget').get(getGoals);
-
-module.exports = router; 
